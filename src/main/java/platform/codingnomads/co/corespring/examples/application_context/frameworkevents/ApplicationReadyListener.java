baab8c65941b488e5c62a00f49a396eb116f880c
Thomas Hill <tomhill555@gmail.com>
2023-02-06 13:53:19 +0000

    completed applicationreadylistener

---
+++ src/main/java/platform/codingnomads/co/corespring/examples/application_context/frameworkevents/ApplicationReadyListener.java
@@ -0,0 +1,14 @@
+package platform.codingnomads.co.corespring.examples.application_context.frameworkevents;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationReadyListener implements ApplicationListener<ApplicationReadyEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        System.out.println("Application Ready!");
+    }
+}
